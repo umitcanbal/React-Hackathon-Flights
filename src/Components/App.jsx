7810f067d7ecae638dbdb3dd3795ed6396e4907f
umitcanbal <umitcanbal@gmail.com>
2020-04-07 17:15:54 +0200

    fetching for a specific search is done

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,8 +3,14 @@ import React from 'react';
 export default class App extends React.Component {
 
     constructor(props) {
-        super(props); // calls the constructor of the extended class
-                      // which is React.Component
+        super(props); 
+    }
+
+    async componentDidMount() {
+        let fetchedData = null;
+        fetchedData = await fetch("https://api.skypicker.com/flights?fly_from=PRG&fly_to=LGW&dateFrom=18/11/2020&dateTo=19/11/2020&partner=picky&v=3")
+            .then(response => response.json())
+            .then(data => (data));
     }
 
     render() {
