9914cbe110e4ad03f1984f3ab2f8644dc67273f8
umitcanbal <umitcanbal@gmail.com>
2020-04-09 03:08:06 +0200

    pagination is implemented, code can definetely be written cleaner, search button search even if the searching parameters remain the same. Apart from them, MILESTONE 4 DONE

--- src/Components/Flights.jsx
+++ src/Components/Flights.jsx
@@ -2,14 +2,50 @@ import React from "react";
 import { DateTime } from 'luxon';
 
 export default class Flights extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentPage: 1,
+      numberOfPages: null,
+    }
+  }
+
+  componentDidMount() {
+    const {data} = this.props;
+    const numberOfPages = Math.ceil(data.length / 5);
+    this.setState({numberOfPages: numberOfPages})
+  }
+
+  componentDidUpdate(prevProps) {
+    const {data} = this.props;
+    if(data !== prevProps.data) {
+      const numberOfPages = Math.ceil(data.length / 5);
+      this.setState({numberOfPages: numberOfPages, currentPage: 1})
+    }
+  }
+
+  previousPage = () => {
+    this.setState({currentPage: this.state.currentPage-1})
+  }
+
+  nextPage = () => {
+    this.setState({currentPage: this.state.currentPage+1})
+  }
+
   render() {
     const {data} = this.props;
+    const {currentPage, numberOfPages} = this.state;
+    console.log("data", data);
+    console.log("currentPage", currentPage);
+    console.log("numberOfPages", numberOfPages);
     if(!data.length) return <p>No flight found for the selected route! Try something else.</p>
-
+    const dataForChosenPage = data.filter( (singleFlight, index) => {
+      if(((currentPage-1)*5)<=index && index<=(currentPage*5-1)) return singleFlight;
+    } )
     return(
       <div className="container">
         <h1>Flight Information..!</h1>
-        <hr/>
         <table className="table table-striped table-bordered table-hover table-condensed">
           <thead>
             <tr>
@@ -23,7 +59,7 @@ export default class Flights extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {data.map((singleFlight, index) => {
+            {dataForChosenPage.map((singleFlight, index) => {
               const {cityFrom, cityTo, dTime, aTime, fly_duration, route, price} = singleFlight;
               const numberOfRoutes = route.length;
               return(
@@ -50,6 +86,8 @@ export default class Flights extends React.Component {
             })}
           </tbody>
         </table>
+        {currentPage!==1 ? <button onClick={this.previousPage}>{"<"}</button> : undefined }
+        {currentPage!==numberOfPages ? <button onClick={this.nextPage}>{">"}</button> : undefined }
       </div>
     )
   }
