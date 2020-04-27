991045d99b0489cc07b166a5d23124d958f79794
umitcanbal <umitcanbal@gmail.com>
2020-04-27 22:33:11 +0200

    code is much cleaner, input fields are their datalists are all handled by one function, both datalists dont appear at the same time, upon users click the correct value is attached to input field etc. To do: styling, components should be put and rendered in their own files, calender can be added maybe return flight option, code can be cleaner

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,90 +1,87 @@
 import React from "react";
-import {MySpinner} from "./Spinner.jsx";
+import { MySpinner } from "./Spinner.jsx";
 import Flights from "./Flights.jsx";
 import Dropdown from "./Dropdown.jsx";
 
-import {fetchCities, fetchFlights} from "../Api"
+import { fetchCities, fetchFlights } from "../Api"
+
 export default class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            isLoading: false,
-            isCheckboxOn: false,
-            data: null,
-            fly_from: null,
-            fly_to: null,
-            departure: "",
-            arrival: "",
-        }
+    state = {
+        isLoading: false,
+        isCheckboxOn: false,
+        data: "",
+        fly_from: "",
+        fly_to: "",
+        departure: "",
+        arrival: "",
     }
 
-    onInputChange = async ({target: {value, name}}) => {  
+    onInputChange = async ({ target: { value, name } }) => {
         const shownCities = await fetchCities(value)
-        this.setState({
-            [name]: shownCities
-        });
-    }
+        this.setState({ [name]: shownCities })
 
-    chooseDepartureCity = () => {
-        const value = event.target.value;
-        this.setState({fly_from: value});
-    }
-
-    chooseArrivalCity = () => {
-        const value = event.target.value;
-        this.setState({fly_to: value});
+        this.state[name].locations.map(location => {
+            if (location.code === value && name === "departure") {
+                this.setState({ fly_from: value })
+            }
+            if (location.code === value && name === "arrival") {
+                this.setState({ fly_to: value })
+            }
+        })
     }
 
     clickCheckBox = () => {
-        this.setState({isCheckboxOn: !this.state.isCheckboxOn})
+        this.setState({ isCheckboxOn: !this.state.isCheckboxOn })
     }
 
     clickSearchButton = () => {
-        const {fly_from, fly_to} = this.state;
-        if(fly_from && fly_to) this.searchFlights();
-        if(!fly_from || !fly_to) alert("Please choose both the destination and the arrival city")
+        const { fly_from, fly_to } = this.state;
+        if (fly_from && fly_to) this.searchFlights();
+        if (!fly_from || !fly_to) alert("Please choose valid destination and arrival city")
     }
 
     async searchFlights() {
-        this.setState({isLoading: true});
-        const {fly_from, fly_to, isCheckboxOn} = this.state;
+        this.setState({ isLoading: true });
+        const { fly_from, fly_to, isCheckboxOn } = this.state;
 
         const fetchedFlightsData = await fetchFlights(fly_from, fly_to, isCheckboxOn)
-        this.setState({data: fetchedFlightsData, isCheckboxOn: isCheckboxOn, isLoading: false})
+        this.setState({ data: fetchedFlightsData, isCheckboxOn: isCheckboxOn, isLoading: false })
     }
 
     render() {
-        const {data, isLoading, isCheckboxOn, departure, arrival} = this.state;    
-        
+        const { data, isLoading, isCheckboxOn, departure, arrival } = this.state;
+
         return (
             <div>
-                <div style={{display: "flex", justifyContent: "center"}}>
-                    <input type="text" placeholder="Departure City" name="departure" onChange={this.onInputChange}/>
-                    <input type="text" placeholder="Arrival City" name="arrival" onChange={this.onInputChange}/>
+                <div style={{ display: "flex", justifyContent: "center" }}>
+                    <input type="text" placeholder="Departure City" name="departure" list="departure" onChange={this.onInputChange} />
+                    <input type="text" placeholder="Arrival City" name="arrival" list="arrival" onChange={this.onInputChange} />
                     <label><input type="checkbox" defaultChecked={isCheckboxOn} onChange={this.clickCheckBox} /> Only direct flights</label>
                     <button onClick={this.clickSearchButton}>Search..!</button>
                 </div>
+                
+                <datalist id="departure" >
+                    {departure ? departure.locations.map((city, index) =>
+                        <option
+                            key={index}
+                            value={`${city.code}`}
+                        >
+                            {city.name}
+                        </option>
+                    ) : undefined}
+                </datalist>
 
-                {departure ? departure.locations.map((city, index) => <option 
-                    key={index} 
-                    name="fly_from" 
-                    value={`${city.code}`} 
-                    onClick={this.chooseDepartureCity}>
-                        {city.name}
-                    </option> 
-                ) : undefined}
-
-                {arrival ? arrival.locations.map((city, index) => <option 
-                    key={index} 
-                    name="fly_to" 
-                    value={`${city.code}`} 
-                    onClick={this.chooseArrivalCity}>
-                        {city.name}
-                    </option> 
-                ) : undefined}
-
+                <datalist id="arrival" >
+                    {arrival ? arrival.locations.map((city, index) =>
+                        <option
+                            key={index}
+                            value={`${city.code}`}
+                        >
+                            {city.name}
+                        </option>
+                    ) : undefined}
+                </datalist>
 
                 {isLoading ? <MySpinner /> : undefined}
                 {data ? <Flights data={data.data} isCheckboxOn={isCheckboxOn} /> : undefined}
@@ -93,7 +90,6 @@ export default class App extends React.Component {
     }
 
 }
-
 
 
 
@@ -121,17 +117,17 @@ export default class App extends React.Component {
 
 
 
-    // chooseCity = () => {
-    //     const value = event.target.innerText;
-        
-    //     cities.departureCities.map( (city) => {
-    //         if(city[value]) this.setState({fly_from: city[value], city_from: value});
-    //     } )    
-    //     cities.arrivalCities.map( (city) => {
-    //         if(city[value]) this.setState({fly_to: city[value], city_to: value});
-    //     } ) 
-    // }
+// chooseCity = () => {
+//     const value = event.target.innerText;
+
+//     cities.departureCities.map( (city) => {
+//         if(city[value]) this.setState({fly_from: city[value], city_from: value});
+//     } )    
+//     cities.arrivalCities.map( (city) => {
+//         if(city[value]) this.setState({fly_to: city[value], city_to: value});
+//     } ) 
+// }
 
 
-                        {/* <Dropdown dropdownTitle={city_from ? city_from : "Departure"} cities={departureCities} chooseCity={this.chooseCity} />
+{/* <Dropdown dropdownTitle={city_from ? city_from : "Departure"} cities={departureCities} chooseCity={this.chooseCity} />
                     <Dropdown dropdownTitle={city_to ? city_to : "Arrival"} cities={arrivalCities} chooseCity={this.chooseCity} /> */}
